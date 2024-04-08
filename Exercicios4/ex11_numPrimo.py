3f66be31eae49775bb82297f3fe9b31f66562428
marcos3777 <marcos_vinicius_11@hotmail.com>
2024-04-08 20:45:06 -0300

    ex11

--- Exercicios4/ex11_numPrimo.py
+++ Exercicios4/ex11_numPrimo.py
@@ -3,8 +3,16 @@
 # ímpares até o número lido. Se o resto de uma dessas divisões for igual a zero, o número não é primo.
 # Observe que 0 e 1 não são primos e que 2 é o único número primo que é par.
 
-num = int (input("Digite o numero: "))
-input("Digite 0 para sair.")
+num = int(input("Digite um número: "))
+if num == 0 or num == 1:
+    print("Não é primo")
+elif num == 2:
+    print("É primo")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Não é primo")
+            break
+    else:
+        print("É primo")
 
-while num != 0:
-    if 
