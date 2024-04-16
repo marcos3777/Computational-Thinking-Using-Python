9fd72dfae42623f56bf14b9d1c4b92da19282f72
marcos3777 <marcos_vinicius_11@hotmail.com>
2024-04-15 21:39:22 -0300

    Exercicios laço feito em sala

--- Exercicios5/ex1_selecOperador.py
+++ Exercicios5/ex1_selecOperador.py
@@ -1,25 +1,27 @@
 import math
 
-print("+ - * /")
-operador = input("Digite o operador: ")
-print("+ - * /")
-num1 = 0
-num2 = 0
+
+cond=0
+
+while cond==0:
 
 
-num1 = float (input("Digite o primeiro número: "))
-num2 = float (input("Digite o segundo número: "))
+    print("+ - * /")
+    operador = input("Digite o operador: ")
+    print("+ - * /")
+    num1 = float (input("Digite o primeiro número: "))
+    num2 = float (input("Digite o segundo número: "))
+
    
 
-    
-
-if operador == "+":
-    print(num1 + num2)
-elif operador == "-":
-    print(num1 - num2)
-elif operador == "*":
-    print(num1 * num2)
-elif operador == "/":
-    print(num1 / num2)
-else:
-    print("Operador inválido")
+    if operador == "+":
+        print(num1 + num2)
+    elif operador == "-":
+        print(num1 - num2)
+    elif operador == "*":
+        print(num1 * num2)
+    elif operador == "/":
+        print(num1 / num2)
+    else:
+        print("Operador inválido")
+    cond = int(input("Digite 0 para continuar ou 1 para sair: "))
